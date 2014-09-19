d3516551b4983a9e7348aaaef6f1c38dee874b8a
Andrew Downes <mrdownes@hotmail.com>
2014-09-19 22:38:25 +0100

    Unmodified storyline files

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 StoryCanJS
 ==========
 
-Modified Storyline files to use TinCanJS
+Modified Storyline files to use TinCanJS.
+
+This repo will include:
+
+* Files from a published Storyline 2 package, copyright Articulate. 
+* TinCanJS by Rustici Software, relased under Apache 2 license - get the latest version at 
+https://github.com/RusticiSoftware/TinCanJS/
+* New code added to repalce functions in the Storyline 2 package files. Copyright Andrew Downes, 
+released under Apache 2 license. 
---
+++ xapi.js
@@ -0,0 +1,109 @@
+
+    var COURSE_ID = '';
+    var XAPI_ACTIVITIES = {"http://http://tincanapi.com/pa2nonlinear/6XVMZachY6V": {"name": {"und": "Lengkapkan 4 strategi Dasar Jaminan Bekalan Makanan berikut."}, "description": {"und": "Lengkapkan 4 strategi Dasar Jaminan Bekalan Makanan berikut."}}, "http://http://tincanapi.com/pa2nonlinear/5ahvCivQWY8/6aEuo9n0SFC": {"name": {"und": "Berikut adalah 4 objektif dalam Dasar Automatif Negara (DAN). Lengkapkan ruangan kosong berikut. "}, "description": {"und": "Berikut adalah 4 objektif dalam Dasar Automatif Negara (DAN). Lengkapkan ruangan kosong berikut. "}}, "http://http://tincanapi.com/pa2nonlinear/6a77C09iDQx": {"name": {"und": "help"}, "description": {"und": "help"}}, "http://http://tincanapi.com/pa2nonlinear/5j2GIiaPCzZ": {"name": {"und": "Dua strategi yang terdapat dalam Dasar Ekonomi Baru (DEB) adalah seperti dibawah. Lengkapkan ruang kosong berikut."}, "description": {"und": "Dua strategi yang terdapat dalam Dasar Ekonomi Baru (DEB) adalah seperti dibawah. Lengkapkan ruang kosong berikut."}}, "http://http://tincanapi.com/pa2nonlinear/6j3FfhyZREU/6AslMzcMAzp": {"name": {"und": "Nyatakan salah satu daripada 3 objektif Dasar Jaminan Bekalan Makanan dengan mengisi tempat kosong berikut."}, "description": {"und": "Nyatakan salah satu daripada 3 objektif Dasar Jaminan Bekalan Makanan dengan mengisi tempat kosong berikut."}}, "http://http://tincanapi.com/pa2nonlinear/6L29giRDMHg/6osfvPJRfEj": {"name": {"und": "Tahap kemajuan sesebuah negara diukur menggunakan 4 indikator berikut. Isikan tempat kosong dibawah."}, "description": {"und": "Tahap kemajuan sesebuah negara diukur menggunakan 4 indikator berikut. Isikan tempat kosong dibawah."}}, "http://http://tincanapi.com/pa2nonlinear/5sIGS6OJqsy": {"name": {"und": "pa2nonlinear"}, "description": {"und": "pa2nonlinear"}}, "http://http://tincanapi.com/pa2nonlinear/62u1AuJoKtO": {"name": {"und": "Tiga objektif Dasar Pertanian Negara (1998-2010) adalah seperti dibawah. Isikan tempat kosong berikut. "}, "description": {"und": "Tiga objektif Dasar Pertanian Negara (1998-2010) adalah seperti dibawah. Isikan tempat kosong berikut. "}}, "http://http://tincanapi.com/pa2nonlinear/5yiSEnY2ZMQ": {"name": {"und": "score"}, "description": {"und": "score"}}, "http://http://tincanapi.com/pa2nonlinear/5uyEImYsu1Q": {"name": {"und": "Lengkapkan salah satu teras daripada 4 teras Pelan Induk Pembangunan Pendidikan (PIPP) berikut."}, "description": {"und": "Lengkapkan salah satu teras daripada 4 teras Pelan Induk Pembangunan Pendidikan (PIPP) berikut."}}, "http://http://tincanapi.com/pa2nonlinear/6JP6ZxR7OMd": {"name": {"und": "Dibawah adalah 3 cabaran dalam Wawasan 2020, Isikan ruangan kosong dibawah."}, "description": {"und": "Dibawah adalah 3 cabaran dalam Wawasan 2020, Isikan ruangan kosong dibawah."}}, "http://http://tincanapi.com/pa2nonlinear/6l8HBjoUsVu/6awOmj6TEdn": {"name": {"und": "Berikut adalah 4 inisiatif pembaharuan strategik dalam Model Baru Ekonomi (MBE). Lengkapkan tempat kosong berikut."}, "description": {"und": "Berikut adalah 4 inisiatif pembaharuan strategik dalam Model Baru Ekonomi (MBE). Lengkapkan tempat kosong berikut."}}, "http://http://tincanapi.com/pa2nonlinear/5ahvCivQWY8": {"name": {"und": "Berikut adalah 4 objektif dalam Dasar Automatif Negara (DAN). Lengkapkan ruangan kosong berikut. "}, "description": {"und": "Berikut adalah 4 objektif dalam Dasar Automatif Negara (DAN). Lengkapkan ruangan kosong berikut. "}}, "http://http://tincanapi.com/pa2nonlinear/6N5t3cwiQz0/6Jyudls4q0u": {"name": {"und": "Dibawah adalah 4 langkah-langkah membina negara maju dan sejahtera. Lengkapkan tempat kosong yang diberikan."}, "description": {"und": "Dibawah adalah 4 langkah-langkah membina negara maju dan sejahtera. Lengkapkan tempat kosong yang diberikan."}}, "http://http://tincanapi.com/pa2nonlinear/6lOBtV1ki3h/5YEP6nxhj3R": {"name": {"und": "Dasar Alam Sekitar Negara mempunyai 8 prinsip. Lengkapkan salah satu prinsip daripada 4 prinsip yang dinyatakan dibawah."}, "description": {"und": "Dasar Alam Sekitar Negara mempunyai 8 prinsip. Lengkapkan salah satu prinsip daripada 4 prinsip yang dinyatakan dibawah."}}, "http://http://tincanapi.com/pa2nonlinear/6qJQViLchfm": {"name": {"und": "Tiga Dasar Pertanian Negara yang telah digubal untuk memajukan sektor pertanian adalah seperti dibawah. Lengkapkan ruangan kosong tersebut. "}, "description": {"und": "Tiga Dasar Pertanian Negara yang telah digubal untuk memajukan sektor pertanian adalah seperti dibawah. Lengkapkan ruangan kosong tersebut. "}}, "http://http://tincanapi.com/pa2nonlinear/60WGuhJlSR6/6ddRcuv7hzx": {"name": {"und": "Huraikan tentang teras keempat Pelan Induk Pembangunan Pendidikan (PIPP)."}, "description": {"und": "Huraikan tentang teras keempat Pelan Induk Pembangunan Pendidikan (PIPP)."}}, "http://http://tincanapi.com/pa2nonlinear/6JP6ZxR7OMd/5ozsdzVrzlY": {"name": {"und": "Dibawah adalah 3 cabaran dalam Wawasan 2020, Isikan ruangan kosong dibawah."}, "description": {"und": "Dibawah adalah 3 cabaran dalam Wawasan 2020, Isikan ruangan kosong dibawah."}}, "http://http://tincanapi.com/pa2nonlinear/62u1AuJoKtO/67caB4dljAK": {"name": {"und": "Tiga objektif Dasar Pertanian Negara (1998-2010) adalah seperti dibawah. Isikan tempat kosong berikut. "}, "description": {"und": "Tiga objektif Dasar Pertanian Negara (1998-2010) adalah seperti dibawah. Isikan tempat kosong berikut. "}}, "http://http://tincanapi.com/pa2nonlinear/6AiAsDrOQqa": {"name": {"und": "Berikut adalah 4 Prinsip Rukun Negara, Isikan ruangan kosong dibawah."}, "description": {"und": "Berikut adalah 4 Prinsip Rukun Negara, Isikan ruangan kosong dibawah."}}, "http://http://tincanapi.com/pa2nonlinear/6E65fWeYMlW/6nqKKvEIIBb": {"name": {"und": "Nyatakan salah satu daripada 3 dasar kerajaan dalam membentuk perpaduan di Malaysia"}, "description": {"und": "Nyatakan salah satu daripada 3 dasar kerajaan dalam membentuk perpaduan di Malaysia"}}, "http://http://tincanapi.com/pa2nonlinear/5i9N36CnEoy/6YNJncRkAiu": {"name": {"und": "Kedayasaingan adalah salah satu daripada dua teras Dasar Agromakanan. Nyatakan satu lagi teras Dasar Agromakanan dalam ruangan kosong berikut."}, "description": {"und": "Kedayasaingan adalah salah satu daripada dua teras Dasar Agromakanan. Nyatakan satu lagi teras Dasar Agromakanan dalam ruangan kosong berikut."}}, "http://http://tincanapi.com/pa2nonlinear/6XVMZachY6V/5tjLeFnTtah": {"name": {"und": "Lengkapkan 4 strategi Dasar Jaminan Bekalan Makanan berikut."}, "description": {"und": "Lengkapkan 4 strategi Dasar Jaminan Bekalan Makanan berikut."}}, "http://http://tincanapi.com/pa2nonlinear/5iUrg19HTWj": {"name": {"und": "help2"}, "description": {"und": "help2"}}, "http://http://tincanapi.com/pa2nonlinear/6SEMAHKkzvG/6XGoSDEQq39": {"name": {"und": "Senaraikan satu daripada 4 bentuk penswastaan dalam Dasar Penswastaan."}, "description": {"und": "Senaraikan satu daripada 4 bentuk penswastaan dalam Dasar Penswastaan."}}, "http://http://tincanapi.com/pa2nonlinear/6q4wdYB2pVO/5rgt5Nk7S6Q": {"name": {"und": "Tiga elemen bagi memastikan Malaysia terus berdaya saing di peringkat global pada tahun 2020. Lengkapkan tempat kosong berikut."}, "description": {"und": "Tiga elemen bagi memastikan Malaysia terus berdaya saing di peringkat global pada tahun 2020. Lengkapkan tempat kosong berikut."}}, "http://http://tincanapi.com/pa2nonlinear/5mnCC3E5GGV": {"name": {"und": "Nyatakan salah satu daripada 3 ciri penting mencapai taraf sogoshosha."}, "description": {"und": "Nyatakan salah satu daripada 3 ciri penting mencapai taraf sogoshosha."}}, "http://http://tincanapi.com/pa2nonlinear/6j3FfhyZREU": {"name": {"und": "Nyatakan salah satu daripada 3 objektif Dasar Jaminan Bekalan Makanan dengan mengisi tempat kosong berikut."}, "description": {"und": "Nyatakan salah satu daripada 3 objektif Dasar Jaminan Bekalan Makanan dengan mengisi tempat kosong berikut."}}, "http://http://tincanapi.com/pa2nonlinear/5mzxBK7xBty": {"name": {"und": "main_menu"}, "description": {"und": "main_menu"}}, "http://http://tincanapi.com/pa2nonlinear/6qJQViLchfm/67RNZKxBn7m": {"name": {"und": "Tiga Dasar Pertanian Negara yang telah digubal untuk memajukan sektor pertanian adalah seperti dibawah. Lengkapkan ruangan kosong tersebut. "}, "description": {"und": "Tiga Dasar Pertanian Negara yang telah digubal untuk memajukan sektor pertanian adalah seperti dibawah. Lengkapkan ruangan kosong tersebut. "}}, "http://http://tincanapi.com/pa2nonlinear/6SEMAHKkzvG": {"name": {"und": "Senaraikan satu daripada 4 bentuk penswastaan dalam Dasar Penswastaan."}, "description": {"und": "Senaraikan satu daripada 4 bentuk penswastaan dalam Dasar Penswastaan."}}, "http://http://tincanapi.com/pa2nonlinear/6q4wdYB2pVO": {"name": {"und": "Tiga elemen bagi memastikan Malaysia terus berdaya saing di peringkat global pada tahun 2020. Lengkapkan tempat kosong berikut."}, "description": {"und": "Tiga elemen bagi memastikan Malaysia terus berdaya saing di peringkat global pada tahun 2020. Lengkapkan tempat kosong berikut."}}, "http://http://tincanapi.com/pa2nonlinear/6AiAsDrOQqa/68u18DF35lN": {"name": {"und": "Berikut adalah 4 Prinsip Rukun Negara, Isikan ruangan kosong dibawah."}, "description": {"und": "Berikut adalah 4 Prinsip Rukun Negara, Isikan ruangan kosong dibawah."}}, "http://http://tincanapi.com/pa2nonlinear/5uyEImYsu1Q/5yNABW4yIHQ": {"name": {"und": "Lengkapkan salah satu teras daripada 4 teras Pelan Induk Pembangunan Pendidikan (PIPP) berikut."}, "description": {"und": "Lengkapkan salah satu teras daripada 4 teras Pelan Induk Pembangunan Pendidikan (PIPP) berikut."}}, "http://http://tincanapi.com/pa2nonlinear/6lOBtV1ki3h": {"name": {"und": "Dasar Alam Sekitar Negara mempunyai 8 prinsip. Lengkapkan salah satu prinsip daripada 4 prinsip yang dinyatakan dibawah."}, "description": {"und": "Dasar Alam Sekitar Negara mempunyai 8 prinsip. Lengkapkan salah satu prinsip daripada 4 prinsip yang dinyatakan dibawah."}}, "http://http://tincanapi.com/pa2nonlinear/6ZgMC6Y8Frq": {"name": {"und": "Results Slide"}, "description": {"und": "Results Slide"}}, "http://http://tincanapi.com/pa2nonlinear/60WGuhJlSR6": {"name": {"und": "Huraikan tentang teras keempat Pelan Induk Pembangunan Pendidikan (PIPP)."}, "description": {"und": "Huraikan tentang teras keempat Pelan Induk Pembangunan Pendidikan (PIPP)."}}, "http://http://tincanapi.com/pa2nonlinear/6a2953Ltzcw/6U3nwzPnJDc": {"name": {"und": "Empat tonggak utama yang menjadi pemacu ekonomi negara untuk mencapai Wawasan 2020 dalam Model Baru Ekonomi (MBE). Lengkapkan tempat kosong dibawah."}, "description": {"und": "Empat tonggak utama yang menjadi pemacu ekonomi negara untuk mencapai Wawasan 2020 dalam Model Baru Ekonomi (MBE). Lengkapkan tempat kosong dibawah."}}, "http://http://tincanapi.com/pa2nonlinear": {"name": {"und": "pa2nonlinear"}, "description": {"und": "Pengajian Am Penggal 2 - nonlinear"}}, "http://http://tincanapi.com/pa2nonlinear/5mnCC3E5GGV/68VyfbtC8pt": {"name": {"und": "Nyatakan salah satu daripada 3 ciri penting mencapai taraf sogoshosha."}, "description": {"und": "Nyatakan salah satu daripada 3 ciri penting mencapai taraf sogoshosha."}}, "http://http://tincanapi.com/pa2nonlinear/5i9N36CnEoy": {"name": {"und": "Kedayasaingan adalah salah satu daripada dua teras Dasar Agromakanan. Nyatakan satu lagi teras Dasar Agromakanan dalam ruangan kosong berikut."}, "description": {"und": "Kedayasaingan adalah salah satu daripada dua teras Dasar Agromakanan. Nyatakan satu lagi teras Dasar Agromakanan dalam ruangan kosong berikut."}}, "http://http://tincanapi.com/pa2nonlinear/6l8HBjoUsVu": {"name": {"und": "Berikut adalah 4 inisiatif pembaharuan strategik dalam Model Baru Ekonomi (MBE). Lengkapkan tempat kosong berikut."}, "description": {"und": "Berikut adalah 4 inisiatif pembaharuan strategik dalam Model Baru Ekonomi (MBE). Lengkapkan tempat kosong berikut."}}, "http://http://tincanapi.com/pa2nonlinear/5Wjcl48b8As": {"name": {"und": "note"}, "description": {"und": "note"}}, "http://http://tincanapi.com/pa2nonlinear/6L29giRDMHg": {"name": {"und": "Tahap kemajuan sesebuah negara diukur menggunakan 4 indikator berikut. Isikan tempat kosong dibawah."}, "description": {"und": "Tahap kemajuan sesebuah negara diukur menggunakan 4 indikator berikut. Isikan tempat kosong dibawah."}}, "http://http://tincanapi.com/pa2nonlinear/6N5t3cwiQz0": {"name": {"und": "Dibawah adalah 4 langkah-langkah membina negara maju dan sejahtera. Lengkapkan tempat kosong yang diberikan."}, "description": {"und": "Dibawah adalah 4 langkah-langkah membina negara maju dan sejahtera. Lengkapkan tempat kosong yang diberikan."}}, "http://http://tincanapi.com/pa2nonlinear/5j2GIiaPCzZ/5ozBG2PgIda": {"name": {"und": "Dua strategi yang terdapat dalam Dasar Ekonomi Baru (DEB) adalah seperti dibawah. Lengkapkan ruang kosong berikut."}, "description": {"und": "Dua strategi yang terdapat dalam Dasar Ekonomi Baru (DEB) adalah seperti dibawah. Lengkapkan ruang kosong berikut."}}, "http://http://tincanapi.com/pa2nonlinear/6a2953Ltzcw": {"name": {"und": "Empat tonggak utama yang menjadi pemacu ekonomi negara untuk mencapai Wawasan 2020 dalam Model Baru Ekonomi (MBE). Lengkapkan tempat kosong dibawah."}, "description": {"und": "Empat tonggak utama yang menjadi pemacu ekonomi negara untuk mencapai Wawasan 2020 dalam Model Baru Ekonomi (MBE). Lengkapkan tempat kosong dibawah."}}, "http://http://tincanapi.com/pa2nonlinear/6E65fWeYMlW": {"name": {"und": "Nyatakan salah satu daripada 3 dasar kerajaan dalam membentuk perpaduan di Malaysia"}, "description": {"und": "Nyatakan salah satu daripada 3 dasar kerajaan dalam membentuk perpaduan di Malaysia"}}};
+
+    var ACTIVITY_ID = getParameters(window.location.search.substr(1))['activity_id'];
+
+
+function getParameters(strData) {
+  var kvs = unescape(strData).split('&'),
+      parameters = {},
+      i, parts;
+
+  for ( i = 0; i < kvs.length; i++ ) {
+    parts = kvs[i].split('=');
+    if ( parts.length < 2 ) {
+      parameters[unescape(parts[0])] = '';
+    } else {
+      parameters[unescape(parts[0])] = unescape(parts.slice(1).join('='));
+    }
+  }
+
+  return parameters;
+}
+
+function putParameters(parameters) {
+  var items = [];
+
+  for ( var key in parameters ) {
+    if ( parameters.hasOwnProperty(key) ) {
+      items.push(escape(key) + '=' + escape(parameters[key]));
+    }
+  }
+
+  return items.join('&');
+}
+
+function setDefinition(obj, id) {
+  if ( id == ACTIVITY_ID ) {
+    id = COURSE_ID;
+  }
+
+  if ( id in XAPI_ACTIVITIES ) {
+    obj.definition = XAPI_ACTIVITIES[id];
+  }
+}
+
+function addContextDefinitions(activities, key) {
+  if ( !(key in activities) ) {
+    return;
+  }
+
+  if ( activities[key].id in XAPI_ACTIVITIES || activities[key].id == ACTIVITY_ID ) {
+    setDefinition(activities[key], activities[key].id);
+  }
+}
+
+function addDefinitions(statement) {
+  if ( statement.object && statement.object.id ) {
+    setDefinition(statement.object, statement.object.id);
+  }
+
+  if ( statement.context && statement.context.contextActivities ) {
+    addContextDefinitions(statement.context.contextActivities, 'grouping');
+    addContextDefinitions(statement.context.contextActivities, 'parent');
+  }
+}
+
+function updateContent(content) {
+  addDefinitions(content);
+  return JSON.stringify(content);
+}
+
+function process(data) {
+  var parameters = getParameters(data);
+
+  if ( 'content' in parameters && !('stateId' in parameters) ) {
+    try {
+      parameters.content = updateContent(JSON.parse(parameters.content));
+    } catch ( e ) {
+      console.log(e);
+    }
+  }
+
+  return putParameters(parameters);
+}
+
+
+// If the flash player is running
+if ( window.SendTinCanRequest ) {
+  var originalSendTinCanRequest = window.SendTinCanRequest;
+  window.SendTinCanRequest = function (nMessageType, strMethod, strData, strUrl, arrHeaders) {
+    return originalSendTinCanRequest(
+      nMessageType,
+      strMethod,
+      escape(process(strData)),
+      strUrl,
+      arrHeaders);
+  };
+}
+
+// If the html5 player is running
+if ( window.ppTinCanComm ) {
+  player.useTinCan = true;
+  var originalPPSendRequest = ppTinCanComm.prototype.SendRequest;
+  ppTinCanComm.prototype.SendRequest = function (request) {
+    request.Data = process(request.Data);
+    return originalPPSendRequest.apply(this, [request]);
+  };
+}
